7f171ac71eae81be3b68885335e1f6a1b2fe9312
Andres Clavijo <andresclavijo0822@gmail.com>
2023-11-25 13:05:40 -0300

    test

--- src/models/newsletterModel.js
+++ src/models/newsletterModel.js
@@ -2,7 +2,7 @@ import { conn } from "../config/conm.js";
 
 const enviarNewsletter = async (email) => {
     try {
-        const [rows] = await conn.query("INSERT INTO `Newsletter` (`id`, `email`) VALUES (NULL, ?)", [email]);
+        const [rows] = await conn.query("INSERT INTO `Newsletter` (`id`, `email`, `fecha`) VALUES (NULL, ?, NOW())", [email]);
         return rows;
     } catch (error) {
         throw error;
