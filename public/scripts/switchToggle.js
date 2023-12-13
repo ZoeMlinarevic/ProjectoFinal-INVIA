ae3863e7528182056746dcf764f138cc9e7af9bb
Zoe Mlinarevic <Zoemmedl@gmail.com>
2023-12-12 21:31:36 -0300

    Subiendo cambios

--- public/scripts/switchToggle.js
+++ public/scripts/switchToggle.js
@@ -1,7 +1,31 @@
-function switchToggle() {
-    const buttonToggle = document.querySelector("#switch")
-    buttonToggle.addEventListener("click", () => {
-        document.body.classList.toggle("lightMode");
-    });
-}
+const myFunction = () => {
+    let element = document.body;
+    element.classList.toggle("lightMode");
+    let theme;
 
+    if(element.classList.contains("lightMode")) {
+        console.log("Light Mode");
+        theme = "light";
+    }
+    else{
+        console.log("darkMode");
+        theme = "dark";
+    }
+    localStorage.setItem("pageTheme", JSON.stringify(theme));
+  } ;
+
+  let getTheme = JSON.parse(localStorage.getItem("pageTheme"));
+  console.log(getTheme);
+
+  if(getTheme === "light") {
+    document.body.classList = "lightMode"
+  }
+
+  
+  const toggleMenu = () => {
+    // Select the menu-smartphone element
+    let menuElement = document.querySelector(".menu-smartphone");
+  
+    // Toggle the class on the menu element to control visibility
+    menuElement.classList.toggle("visible");
+  }; 
